b0bf4a2650689cdc4ff71619c8c157cc88ef3e72
HDJRijnders <h.d.j.rijnders@st.hanze.nl>
2019-01-24 11:16:26 +0100

    Goed doorgelinkt
    Stelletje klaptosti's

--- html/header.php
+++ html/header.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="Content-Language" content="en">
     <meta name="language" content="en">
-    <link href="weerstation_style.css" type="text/css" rel="stylesheet">
+    <link href="../weerstation_style.css" type="text/css" rel="stylesheet">
 
     <!-- include login.php-->
     <?php include'scripts/login.php';?>
@@ -12,16 +12,16 @@
     <div class="dropdown" style="float:right;">
       <button class="dropbtn">Menu</button>
       <div class="dropdown-content">
-        <a href="pages/frm_login.php">Inloggen</a>
-        <a href='Pagina 1'>Pagina 1</a>
-        <a href='Pagina 2'>Pagina 2</a>
+        <a href="../pages/frm_login.php">Inloggen</a>
+        <a href='../pages/'>Pagina 1</a>
+        <a href='../pages/'>Pagina 2</a>
       </div>
     </div>
 
     <div class="header">
       <div class="container">
         <p ALIGN="center">
-       ITV2B-04 Website Project Weerstation
+            <a href="../index.php">TV2B-04 Website Project Weerstation</a>
         </p>
       </div>
     </div>
