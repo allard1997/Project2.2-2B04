a822cc3b4c7c02ef809545daaaee13eb30248d5f
Allard <allard1997@users.noreply.github.com>
2019-01-24 14:07:46 +0100

    Fix tags in header.php

--- html/header.php
+++ html/header.php
@@ -1,34 +1,31 @@
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title>ITV2B-04 - Weather stations</title>
-    <meta charset="utf-8">
-    <meta http-equiv="Content-Language" content="en">
-    <meta name="language" content="en">
-    <link href="../weerstation_style.css" type="text/css" rel="stylesheet">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>ITV2B-04 - Weather stations</title>
+  <meta charset="utf-8">
+  <meta http-equiv="Content-Language" content="en">
+  <meta name="language" content="en">
+  <link href="../weerstation_style.css" type="text/css" rel="stylesheet">
+</head>
+<!-- include login.php-->
+<?php include'scripts/login.php';?>
 
-    <!-- include login.php-->
-    <?php include'scripts/login.php';?>
-
-    <div class="dropdown" style="float:right;">
-      <button class="dropbtn">Menu</button>
-      <div class="dropdown-content">
-        <a href="../pages/frm_login.php">Login</a>
-        <a href='../pages/'>Placeholder 1</a>
-        <a href='../pages/'>Placeholder 2</a>
-      </div>
-    </div>
-
-    <div class="header">
-      <div class="container">
-        <p ALIGN="center">
-            <a href="../index.php">ITV2B-04 - Weather stations</a>
-        </p>
-      </div>
-    </div>
-
-    <div class="startpage">
-      <div class="container">
-        <h1>Weather stations</h1>
-      </div>
-    </div>
-  </head>
+<div class="dropdown" style="float:right;">
+  <button class="dropbtn">Menu</button>
+  <div class="dropdown-content">
+    <a href="../pages/frm_login.php">Login</a>
+    <a href='../pages/'>Placeholder 1</a>
+    <a href='../pages/'>Placeholder 2</a>
+  </div>
+</div>
+<div class="header">
+  <div class="container">
+    <p ALIGN="center">
+        <a href="../index.php">ITV2B-04 - Weather stations</a>
+    </p>
+  </div>
+</div>
+<div class="startpage">
+  <div class="container">
+    <h1>Weather stations</h1>
+  </div>
+</div>
