8f3a3150b04705783f35d981af6ef2bf299813fc
Allard <allard1997@users.noreply.github.com>
2019-01-25 13:48:20 +0100

    Top 5 progress + added example CSV

--- html/pages/top5.php
+++ html/pages/top5.php
@@ -7,10 +7,23 @@
 <body>
     <div class=main>
         <div class=container>
-            <h2>Top 5 list - Example</h2>
-            <ol type=1>
-            <li>Example</li>
-            </ol>
+        <h2>Top 5 list - Example</h2>
+        <?php
+            $row = 1;
+            if (($handle = fopen("../../data/2019-01-24_21.csv", "r")) !== FALSE) {
+                echo '<table border="1"><tbody>';
+                while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                    if ($row == 1) {
+                        echo '<thead><tr><th>'.$data[0].': Station nr</th><th>'.$data[1].': Date</th><th>'.$data[2].': Tijd</th><th>'.$data[3].': Temp</th><th>'.$data[7].': Cloud cover</th><th>'.$data[9].': Rainfall</th></tr></thead>';
+                        $row++;
+                    }else{
+                        echo '<tr><td>'.$data[0].'</td><td>'.$data[1].'</td><td>'.$data[2].'</td><td>'.$data[3].'</td><td>'.$data[7].'</td><td>'.$data[9].'</td></tr>';
+                    }
+                }
+                echo '</tbody></table>';
+                fclose($handle);
+            }
+        ?>
         </div>
     </div>
 </body>
