65937235f9f1e312d4a0660303174d5242df79c5
Allard <allard1997@users.noreply.github.com>
2019-01-30 13:54:01 +0100

    Update top5.php + Added dropdown
    Er is nu een keuze menu die de afgelopen 7 dagen aangeeft en na het selecteren de data van de aangegeven dag ophaalt.

--- html/pages/top5.php
+++ html/pages/top5.php
@@ -11,7 +11,7 @@
 <body>
     <div class=main>
         <div class=container>
-        <h2>Top 5 list - Example</h2>
+        <h2>Top 5 list - W.I.P.</h2>
         <?php
             // $row = 1;
             // if (($handle = fopen("../../data/2019-01-24_21.csv", "r")) !== FALSE) {
@@ -30,8 +30,36 @@
         ?>
 
         <?php
-            function readCSV($file)
-            {
+            $SetDate = $_POST["SetDate"];
+            $dir = '../../data';
+            $scan_dir = array_diff(scandir($dir), array('..', '.'));
+            rsort($scan_dir);
+        ?>
+        <form action="top5.php" method="post">
+        Selecteer een datum:
+        <select name="SetDate">
+        <?php
+            $x=0;
+            foreach($scan_dir as $scan_dirs) {
+                if ($SetDate == $scan_dir[$x]) {
+                    echo "<option value=$scan_dir[$x] selected>$scan_dir[$x]</option>";
+                    $x++;
+                }
+                elseif  ($x < 7) {
+                    echo "<option value=$scan_dir[$x]>$scan_dir[$x]</option>";
+                    $x++;
+                } else {
+                    break;
+                }
+            }
+        ?>
+        <input style="margin:5px" type="submit" value="Select">
+        </select>
+        </form>
+
+        <?php
+        function readCSV($file)
+        {
             $row = 0;
             $csvArray = array();
             if( ( $handle = fopen($file, "r") ) !== FALSE ) {
@@ -50,10 +78,13 @@
                 return false;
                 fclose($handle);
             }
+        }
+            if (isset($SetDate)) {
+                $csvData = readCSV("../../data/$SetDate/2019-01-24_21.csv");
+            } else {
+                $csvData = readCSV("../../data/$scan_dir[0]/2019-01-24_21.csv");
             }
-
-            $csvData = readCSV("../../data/2019-01-24_21.csv");
-
+            
             foreach ($csvData as $key => $row) {
                 $temp[$key]  = $row[3];
                 $visib[$key] = $row[7];
@@ -62,51 +93,6 @@
 
             array_multisort($temp, SORT_DESC, $visib, SORT_ASC, $prcp, SORT_ASC, $csvData);
 
-            echo "Top 5 - Temp";
-            $count=0;
-            echo "<table border='1'><thead><tr><th>Station nr</th><th>Date</th><th>Time</th><th>Temp</th><th>Cloud Cover</th><th>Rainfall</th></tr></thead>";
-            foreach ($csvData as $csvData) {
-                if ($count < 5) {
-                    echo "<tr><td>".$csvData[0]."</td><td>".$csvData[1]."</td><td>".$csvData[2]."</td><td>".$csvData[3]."</td><td>".$csvData[7]."</td><td>".$csvData[9]."</td></tr>";
-                $count++;
-                } else {
-                    echo "</tbody></table>";
-                }
-            }
-
-            $csvData = readCSV("../../data/2019-01-24_21.csv");
-
-            foreach ($csvData as $key => $row) {
-                $temp[$key]  = $row[3];
-                $visib[$key] = $row[7];
-                $prcp[$key] = $row[9];
-            }
-
-            array_multisort($visib, SORT_DESC, $temp, SORT_ASC, $prcp, SORT_ASC, $csvData);
-
-            echo "Top 5 - Visib";
-            $count=0;
-            echo "<table border='1'><thead><tr><th>Station nr</th><th>Date</th><th>Time</th><th>Temp</th><th>Cloud Cover</th><th>Rainfall</th></tr></thead>";
-            foreach ($csvData as $csvData) {
-                if ($count < 5) {
-                    echo "<tr><td>".$csvData[0]."</td><td>".$csvData[1]."</td><td>".$csvData[2]."</td><td>".$csvData[3]."</td><td>".$csvData[7]."</td><td>".$csvData[9]."</td></tr>";
-                $count++;
-                } else {
-                    echo "</tbody></table>";
-                }
-            }
-
-            $csvData = readCSV("../../data/2019-01-24_21.csv");
-
-            foreach ($csvData as $key => $row) {
-                $temp[$key]  = $row[3];
-                $visib[$key] = $row[7];
-                $prcp[$key] = $row[9];
-            }
-
-            array_multisort($prcp, SORT_DESC, $temp, SORT_ASC, $visib, SORT_ASC, $csvData);
-
-            echo "Top 5 - Prcp";
             $count=0;
             echo "<table border='1'><thead><tr><th>Station nr</th><th>Date</th><th>Time</th><th>Temp</th><th>Cloud Cover</th><th>Rainfall</th></tr></thead>";
             foreach ($csvData as $csvData) {
