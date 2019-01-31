85f081bc8b3bcd56c8c5341e3d8f4ad177fb7fee
HDJRijnders <h.d.j.rijnders@st.hanze.nl>
2019-01-31 13:46:45 +0100

    Receiver omgezet in Functies
    Het zijn nu twee aparte functies, fix header en automatisch inlezen pl0x

--- html/scripts/receiver.php
+++ html/scripts/receiver.php
@@ -1,46 +1,52 @@
 <!DOCTYPE html>
 <?php
+//Functie readCSV: leeft een bestand in.
             function readCSV($file)
             {
-            $row = 0;
-            $csvArray = array();
-            if( ( $handle = fopen($file, "r") ) !== FALSE ) {
-                while( ( $data = fgetcsv($handle, 1000, ",") ) !== FALSE ) {
-                $num = count($data);
-                for( $c = 0; $c < $num; $c++ ) {
-                    $csvArray[$row][] = $data[$c];
+                $row = 0;
+                $csvArray = array();
+                if (($handle = fopen($file, "r")) !== FALSE) {
+                    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                        $num = count($data);
+                        for ($c = 0; $c < $num; $c++) {
+                            $csvArray[$row][] = $data[$c];
+                        }
+                        $row++;
+                    }
                 }
-                $row++;
+                if (!empty($csvArray)) {
+                    return array_splice($csvArray, 1);
+                    fclose($handle);
+                } else {
+                    return false;
+                    fclose($handle);
                 }
             }
-            if( !empty( $csvArray ) ) {
-                return array_splice($csvArray, 1);
-                fclose($handle);
-            } else {
-                return false;
-                fclose($handle);
+
+//Functie writeCSV: schrijft het ingelezen bestand in subbestandjes
+			function writeCSV($file)
+            {
+                $date = date('Y-m-d');
+                if (!file_exists("../../data/" . $date)) {
+                    mkdir("../../data/" . $date, 0777, true);
+                }
+                foreach ($file as $csvSplitContents) {
+                    print_r($csvSplitContents);
+                    if (!file_exists($csvSplitContents[0])) {
+                        fopen("../../data/" . $date . "/" . $csvSplitContents[0] . ".csv", "a");
+                        {
+                            $file1 = fopen("../../data/" . $date . "/" . $csvSplitContents[0] . ".csv", "a");
+                            fputcsv($file1, $csvSplitContents);
+                        }
+                    }
+                }
             }
-			
-			  
-			}
-			$arr = array('hoi', 'hallo');
-			$csvData = readCSV("../../data/2019-01-24_21.csv");
-			$date = date('Y-m-d');
-			
-			if (!file_exists("../../data/".$date)) {
-				mkdir("../../data/".$date, 0777, true);
-			}
-			
-			foreach ($csvData as $csvData) {
-				echo $csvData[0], ',';
-				if (!file_exists($csvData[0])) {
-					fopen("../../data/".$date."/".$csvData[0].".csv", "a");	{
-						$file1 = fopen("../../data/".$date."/".$csvData[0].".csv", "a");
-						fputcsv($file1, $csvData);
-					}
-				}
-			}
-						    			
+
+
+//Test, maak hier het wel automatisch van
+
+$testlees = readCSV("../../data/2019-01-24_21.csv");
+$testwrite = writeCSV($testlees);
 ?>
         
         </div>
