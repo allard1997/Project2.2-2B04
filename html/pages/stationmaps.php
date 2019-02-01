4483d17a97d807f0ed7ebfa8544227b6dbc98432
HDJRijnders <h.d.j.rijnders@st.hanze.nl>
2019-02-01 12:42:16 +0100

    Map werkt
    Nu alleen nog de pins!

--- html/pages/stationmaps.php
+++ html/pages/stationmaps.php
@@ -23,14 +23,14 @@ include("../header.php");
     </style>
 
     <script>
-
-        var stationsmap = L.map('map').setView([51.505, -0.09],13);
-        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        
+        var stationsmap = L.map('map').setView([52.51275, 13.38186],26);
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiaGRqcmlqbmRlcnMiLCJhIjoiY2pybHoxaXBxMDJ3ZjQzdXdyY3FjNHY0aSJ9.2tBBcsjliUoQgxwlUhmgtA', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
             maxZoom: 18,
             id: 'mapbox.streets',
             accessToken: 'your.mapbox.access.token'
-        }).addTo(mymap);
+        }).addTo(stationsmap);
 
     </script>
 
