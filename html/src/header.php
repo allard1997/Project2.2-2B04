6c9e71d69a235fda96fd7ece5d7c35726d2a7688
Allard <allard1997@users.noreply.github.com>
2019-02-06 11:47:00 +0100

    Update header.php

--- html/src/header.php
+++ html/src/header.php
@@ -1,32 +1,28 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Weather information</a>
+<a class="navbar-brand" href="#">
+    <img src="https://material.io/tools/icons/static/icons/baseline-wb_sunny-24px.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+    Weather information
+  </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <div class="collapse navbar-collapse">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Home</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
+        <a class="nav-link" href="station_overview.php">Search station</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        <a class="nav-link" href="solar_top5.php">Inefficient locations</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Potential location map</a>
       </li>
     </ul>
     <a href="login.php" class="btn btn-outline-dark my-2 my-sm-0">Login</a>
   </div>
 </nav>
+</br>
