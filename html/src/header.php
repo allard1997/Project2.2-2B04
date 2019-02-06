56e51560146112ae62e415b67e2dfe7cea614964
HDJRijnders <hylbrenrijnders@hotmail.com>
2019-02-06 15:27:40 +0100

    POSSIBLE FIX

--- html/src/header.php
+++ html/src/header.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">
+    <a class="navbar-brand" href="../../index.php">
     <img src="https://material.io/tools/icons/static/icons/baseline-wb_sunny-24px.svg" width="30" height="30" class="d-inline-block align-top" alt="">
     Weather information
   </a>
@@ -12,7 +12,7 @@
   <div class="collapse navbar-collapse" id="navbarContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item">
-        <a class="nav-link" href="index.php">Home</a>
+        <a class="nav-link" href="../../index.php">Home</a>
       </li>
         <?php if(isset($_SESSION['username']))	{ ?>
       <li class="nav-item">
@@ -28,6 +28,6 @@
       <a href="src/scripts/logout.php" class="btn btn-outline-dark my-2 my-sm-0">Logout</a>
       <? }else{?>
     <a href="src/pages/form_login.php" class="btn btn-outline-dark my-2 my-sm-0">Login</a>
-      <? } ?>
+      <? } ;?>
   </div>
 </nav>
