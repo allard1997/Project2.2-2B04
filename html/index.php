7e9c0e326908bbdfc4aaf9524e17b3f30d2ea6e1
HDJRijnders <hylbrenrijnders@hotmail.com>
2019-02-06 12:01:38 +0100

    Index aangepast

--- html/index.php
+++ html/index.php
@@ -19,27 +19,32 @@ include('src/header.php');
             <div class="container">
                 <h1 class="display-3">Weather Information</h1>
                 <p>Webinterface for the Weather Information project for Aitken Space</p>
+
+                <? //als niet logged in: laat login zien ?>
                 <p><a class="btn btn-primary btn-lg" href="/src/login.php" role="button">Login &raquo;</a></p>
+                <? //als wel logged in: laat user zien ?>
+
             </div>
         </div>
 
+        <? //als wel logged in?>
         <div class="container">
             <!-- Example row of columns -->
             <div class="row">
                 <div class="col-md-4">
-                    <h2>Heading</h2>
-                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                    <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+                    <h2>Map</h2>
+                    <p>A map with toggable markers.</p>
+                    <a class="btn btn-secondary" href="pages/stationmaps.php" role="button">View map &raquo;</a></p>
                 </div>
                 <div class="col-md-4">
-                    <h2>Heading</h2>
-                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                    <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+                    <h2>Charts</h2>
+                    <p>Various charts with data</p>
+                    <a class="btn btn-secondary" href="charts.php" role="button">View charts &raquo;</a></p>
                 </div>
                 <div class="col-md-4">
-                    <h2>Heading</h2>
-                    <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-                    <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+                    <h2>Top 5</h2>
+                    <p>Shows the top 5 as specified by Aitken Space</p>
+                    <a class="btn btn-secondary" href="solar_top5.php" role="button">View top 5 &raquo;</a></p>
                 </div>
             </div>
 
