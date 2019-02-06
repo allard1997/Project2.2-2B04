6b766d93db61e503790e89bcf3afc67f4b031d1f
Allard <allard1997@users.noreply.github.com>
2019-02-06 14:03:43 +0100

    Hotfix solar_top5.php

--- html/solar_top5.php
+++ html/solar_top5.php
@@ -7,9 +7,6 @@
     <title>Top 5</title>
     <?php
         include 'src/helper.php';
-        ini_set('display_startup_errors',1);
-        ini_set('display_errors',1);
-        error_reporting(-1);
         $setDate = $_POST["setDate"];
     ?>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
