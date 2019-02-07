d0e25676030e91cf4cda5a47b08343c7ed833276
Allard <allard1997@users.noreply.github.com>
2019-02-07 01:24:05 +0100

    Revert "Revert "Weg met de error displays""
    This reverts commit 9c30a58e3e68cfe5a6f90ab091c7c440e625d9d2.

--- html/top5_resort.php
+++ html/top5_resort.php
@@ -1,9 +1,6 @@
 <?php
 include 'src/model/Station.php';
 include 'src/helper.php';
-ini_set('display_startup_errors',1);
-ini_set('display_errors',1);
-error_reporting(-1);
 
 
 foreach (stations() as $station) {
